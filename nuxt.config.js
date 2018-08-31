9b72f11c86f93a370e62a4755354d58842a3b09e
spiritAndGhost <yt2793240434@163.com>
2018-08-31 22:22:50 +0800

    添加axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,19 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt_project',
+    title: "nuxt_project",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'learn nuxt' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "learn nuxt" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  css: ["~/assets/styles/css/main.css"],
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -24,17 +23,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
         })
       }
-    }
+    },
   },
-  css: ['~/assets/styles/main.css'],
 }
-
--- pages/register/index.vue
+++ pages/register/index.vue
@@ -1,15 +1,59 @@
 <template>
-    <div>我是注册页面</div>
+    <div class="container">
+        <input type="text" >
+        <button class="register-button button" @click="registerButton" v-if="false">注册</button>
+        <button class=" button" @click="getMarketData">请求</button>
+    </div>
 </template>
 
 <script>
+import qs from "qs";
+// import axios from "axios";
+import { getData } from '~/plugins/axios.js'
+import encrypt from "~/utils/rsa.js";
 export default {
-    
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        registerButton() {
+            //do注册
+            // var userName="13087006827";
+            // var code="156966";
+            // var password=encrypt(123456);
+            // var passwordAgain=encrypt(156321);
+            // var agreement=true;
+            // axios.post("https:www.jexzh.com/api/v2/inner/user/regist",qs.stringify({
+            //     userName,code,password,passwordAgain,agreement,
+            // })).then(res=>{
+            // if(res.data && res.data.data && res.data.code == 0){
+            //     console.log(res.data.data);
+            // }
+            // }).catch(error=>{
+            //     console.log(error);
+            // })
+        },
+        getMarketData() {
+             getData('api/v2/inner/market/coin/ticker','get','').then( res => {
+                console.log(res)
+             }), err => {
+                console.log(err)
+             }
+        }
+    },
 }
 </script>
 
 <style scoped>
-
+.register-button, .button {
+    width: 200px;
+    height: 40px;
+    background: #00c1de;
+    border-radius: 2px;
+    border: none;
+}
 </style>
 
 
